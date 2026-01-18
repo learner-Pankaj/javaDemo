32ca8c47f49c7702cea9d073694c9f441695b783
Pankaj <ydvpnkj28@gmail.com>
2026-01-18 21:41:00 +0530

    compare to method used

---
+++ Compare.java
@@ -0,0 +1,9 @@
+package javaDemo;
+public class Compare{
+    public static void main(String [] args){
+        String s1="Pankaj";
+        String s2="Pankaj ";
+        int b = s1.compareTo(s2);
+        System.out.println(b);
+    }
+}
